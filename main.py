a5b3061cbade5ad7f62823473b187a4133e4c62c
luckyof <lucky_redo@mail.ru>
2026-02-27 09:41:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 from gui.main_window import MainWindow
+from core.config import ConfigManager
 
 if __name__ == "__main__":
-    app = MainWindow()
+    # 1. Инициализация менеджера конфигурации
+    config = ConfigManager()
+    
+    # 2. Запуск главного окна с передачей конфига
+    app = MainWindow(config=config)
     app.mainloop()
